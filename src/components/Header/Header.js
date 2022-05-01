678566dcaacbfb634cec7cd6407cdc72955aa8b9
Rana Arju <arjurana530@gmail.com>
2022-05-01 13:19:50 +0600

    primary bootstraping and navbar add

---
+++ src/components/Header/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button, Container, Nav, Navbar} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
+const Header = () => {
+    return (
+    <Navbar bg="primary" variant="dark" expand="lg">
+    <Container> 
+    <img class="w-10 h-10 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="Rounded avatar" />
+
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }}navbarScroll
+      >
+    <LinkContainer to='/'>
+        <Nav.Link>Home</Nav.Link>
+    </LinkContainer>
+    <LinkContainer to='/addProduct'>
+        <Nav.Link>Add Product</Nav.Link>
+    </LinkContainer>
+    <LinkContainer to='/blog'>
+        <Nav.Link>Blog</Nav.Link>
+    </LinkContainer>
+      </Nav>
+      <Nav className="ms-auto my-2 my-lg-0" style={{ maxHeight: '100px' }}navbarScroll
+      >
+        <Nav.Link href="#action1">Login/Register</Nav.Link>
+        <Nav.Link href="#action1">Logout</Nav.Link>
+        <img class="w-10 h-10 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="Rounded avatar" />
+        </Nav>
+     
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+    );
+};
+
+export default Header;
