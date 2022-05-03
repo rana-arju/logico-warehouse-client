485a10cad7ea947c21830faf9574bf0717c3f9d9
Rana Arju <arjurana530@gmail.com>
2022-05-03 23:54:07 +0600

    testimonial section create using  html,css, bootstrap with  fully responsive

---
+++ src/components/Testimonial/Testimonial.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import "./Testimonial.css";
+const Testimonial = () => {
+    return (
+<div className=' testimonial-section py-5'>
+    <h2 className='text-center mb-5 text-primary uppercase' >Our Client Review</h2>
+<div className="container">
+    <div className="row g-2">
+        <div className="col-md-4">
+            <div className="card p-3 text-center px-4">
+        <div className="user-image">
+            <img src="https://i.imgur.com/PKHvlRS.jpg" className="rounded-circle" width="80" alt='..' />
+        </div>
+        <div className="user-content">
+            <h5 className="mb-0">Bruce Hardy</h5>
+            <span>Software Developer</span>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>       
+        </div>
+    </div>
+    </div>
+    <div className="col-md-4">
+            <div className="card p-3 text-center px-4">
+                
+                <div className="user-image">
+                    
+            <img src="https://i.imgur.com/w2CKRB9.jpg" className="rounded-circle" width="80" alt='..' />
+                </div>
+                <div className="user-content">
+                    
+                    <h5 className="mb-0">Mark Smith</h5>
+                    <span>Web Developer</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    
+                </div>
+                
+            </div>
+            
+        </div>
+        
+        <div className="col-md-4">
+            
+            <div className="card p-3 text-center px-4">
+                
+                <div className="user-image">
+                    
+            <img src="https://i.imgur.com/ACeArwY.jpg" className="rounded-circle" width="80" alt='..' />
+                </div>
+                <div className="user-content">
+                    <h5 className="mb-0">Veera  Duncan</h5>
+                    <span>Software Architect</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div> 
+        </div>
+    </div>
+</div>
+</div>
+    );
+};
+
+export default Testimonial;
