60d03d886424a5f91efbfaa94008e26903578aa8
Md Fahad <fahadbdctglh8@gmail.com>
2023-01-11 21:57:22 +0600

    Deplope the header and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,120 +1,146 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { BsGithub, BsFacebook, BsLinkedin, BsInstagram, BsTwitter, BsFillTelephoneFill} from 'react-icons/bs';
-import { AiTwotoneHome, AiOutlineMail} from 'react-icons/ai';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import {
+  BsGithub,
+  BsFacebook,
+  BsLinkedin,
+  BsInstagram,
+  BsTwitter,
+  BsFillTelephoneFill,
+} from "react-icons/bs";
+import { AiTwotoneHome, AiOutlineMail } from "react-icons/ai";
 
 const Footer = () => {
-return (
-<div  className=" border-t-2"  style={{backgroundColor: '#FAFAFA'}}>
-<Container>       
+  return (
+    <div className=" border-t-2">
+      <Container>
+        <footer className="text-center text-lg-start  text-muted">
+          {/* Section: Social media  */}
+          <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+            <div className="me-5 d-none d-lg-block">
+              <span>Get connected with us on social networks:</span>
+            </div>
+            <div className="d-flex">
+              <a
+                href="https://www.facebook.com/ranaarju1"
+                className="me-4 text-reset"
+              >
+                <BsFacebook />
+              </a>
+              <a href=".." className="me-4 text-reset">
+                <BsTwitter />
+              </a>
+              <a href=".." className="me-4 text-reset">
+                <BsInstagram />
+              </a>
+              <a href=".." className="me-4 text-reset">
+                <BsLinkedin />
+              </a>
+              <a
+                href="https://github.com/rana-arju"
+                className="me-4 text-reset"
+              >
+                <BsGithub />
+              </a>
+            </div>
+          </section>
 
-<footer className="text-center text-lg-start  text-muted">
-   {/* Section: Social media  */}
-  <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom" >
+          {/*  Section: Links  */}
+          <section className="">
+            <div className="text-center text-md-start mt-5">
+              <div className="row mt-3">
+                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                  <h4 className="text-uppercase fw-bold mb-4">Logiaca</h4>
+                  <p>
+                    we provide fast service and new smartphone from our
+                    warehouse. genuine products. EMI facility.nationwide
+                    delivery.
+                  </p>
+                </div>
 
-    <div className="me-5 d-none d-lg-block">
-      <span>Get connected with us on social networks:</span>
-    </div>
-    <div className='d-flex'>
-      <a href="https://www.facebook.com/ranaarju1" className="me-4 text-reset">
-        <BsFacebook />
-      </a>
-      <a href=".." className="me-4 text-reset">
-        <BsTwitter />
-      </a>
-      <a href=".." className="me-4 text-reset">
-        <BsInstagram />
-      </a>
-      <a href=".." className="me-4 text-reset" >
-        <BsLinkedin />
-      </a>
-      <a href="https://github.com/rana-arju" className="me-4 text-reset" >
-        <BsGithub />
-      </a>
-    </div>
-  </section>
+                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+                  <p>
+                    <a href="/" className="text-reset">
+                      Realme
+                    </a>
+                  </p>
+                  <p>
+                    <a href="/" className="text-reset">
+                      Radme
+                    </a>
+                  </p>
+                  <p>
+                    <a href="/" className="text-reset">
+                      iphone
+                    </a>
+                  </p>
+                  <p>
+                    <a href="/" className="text-reset">
+                      Samsung
+                    </a>
+                  </p>
+                </div>
 
-{/*  Section: Links  */ }
-  <section className="">
-    <div className="text-center text-md-start mt-5">
+                {/* Grid column */}
+                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                  {/* Links */}
+                  <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+                  <p>
+                    <Link to="/myproduct" className="text-reset">
+                      My Product
+                    </Link>
+                  </p>
+                  <p>
+                    <Link to="/manageInventory" className="text-reset">
+                      Management
+                    </Link>
+                  </p>
+                  <p>
+                    <Link to="/addProduct" className="text-reset">
+                      Add Product
+                    </Link>
+                  </p>
+                  <p>
+                    <Link to="/login" className="text-reset">
+                      Login/Reg
+                    </Link>
+                  </p>
+                </div>
 
-      <div className="row mt-3">
-        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <h4 className="text-uppercase fw-bold mb-4">
-            Logiaca
-          </h4>
-          <p>
-            we provide fast service and new smartphone from our warehouse. genuine products. EMI facility.nationwide delivery.
-          </p>
-        </div>
-
-        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-          <h6 className="text-uppercase fw-bold mb-4">
-            Products
-          </h6>
-          <p>
-            <a href="/" className="text-reset">Realme</a>
-          </p>
-          <p>
-            <a href="/" className="text-reset">Radme</a>
-          </p>
-          <p>
-            <a href="/" className="text-reset">iphone</a>
-          </p>
-          <p>
-            <a href="/" className="text-reset">Samsung</a>
-          </p>
-        </div>
-
-        {/* Grid column */ }
-        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-          {/* Links */}
-          <h6 className="text-uppercase fw-bold mb-4">
-            Useful links
-          </h6>
-          <p>
-            <Link to="/myproduct" className="text-reset">My Product</Link>
-          </p>
-          <p>
-            <Link to="/manageInventory" className="text-reset">Management</Link>
-          </p>
-          <p>
-            <Link to="/addProduct" className="text-reset">Add Product</Link>
-          </p>
-          <p>
-            <Link to="/login" className="text-reset">Login/Reg</Link>
-          </p>
-        </div>
-
-       { /* Grid column */}
-        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-         { /* Links */}
-          <h6 className="text-uppercase fw-bold mb-4">
-            Contact
-          </h6>
-          <p className='d-flex'><AiTwotoneHome /> <span className='ms-2'>Cox's Bazar</span>
-          </p>
-          <p className='d-flex'><AiOutlineMail /> <span className='ms-2'>ranaarju20@gmail.com</span> </p>
-        <p className='d-flex'><BsFillTelephoneFill /> <span className='ms-2'>08801881220413</span> </p>
-        </div>
-       { /* Grid column */}
+                {/* Grid column */}
+                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                  {/* Links */}
+                  <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+                  <p className="d-flex">
+                    <AiTwotoneHome /> <span className="ms-2">Cox's Bazar</span>
+                  </p>
+                  <p className="d-flex">
+                    <AiOutlineMail />{" "}
+                    <span className="ms-2">Computer19-20@gmail.com</span>{" "}
+                  </p>
+                  <p className="d-flex">
+                    <BsFillTelephoneFill />{" "}
+                    <span className="ms-2">08801881220413</span>{" "}
+                  </p>
+                </div>
+                {/* Grid column */}
+              </div>
+            </div>
+          </section>
+          {/* Section: Links  */}
+        </footer>
+      </Container>
+      {/* Copyright */}
+      <div
+        className="text-center p-4 font-sans font-bold text-[20px] text-[#000000ee]"
+        style={{ backgroundColor: "#BFC9CA", width: "100%" }}
+      >
+        &copy; 2022 Copyright: ComputerDeferment-Batch^19-20
       </div>
-   
     </div>
-  </section>
-  {/* Section: Links  */}
-
-  {/* Copyright */}
-  <div className="text-center p-4" style={{backgroundColor: "rgba(0, 0, 0, 0.05)"}}>
-    &copy; 2022 Copyright:
-    <a className="text-reset fw-bold" href="https://github.com/rana-arju"> Rana Arju</a>
-  </div>
-
-</footer>
-</Container>
-</div>
-    );
+  );
 };
 
 export default Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,68 +1,110 @@
-import { signOut } from 'firebase/auth';
-import React from 'react';
-import {Container, Nav, Navbar} from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap'
-import auth from '../../firebase.init';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from "firebase/auth";
+import React, { useState } from "react";
+import { Container } from "react-bootstrap";
+import auth from "../../firebase.init";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { NavLink } from "react-router-dom";
+import { BiLogIn } from "react-icons/bi";
+import { FaTimes } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineLogout } from "react-icons/ai";
+
 const Header = () => {
-const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
+  const [Menu, setMenu] = useState(false);
+  const [UserInfo, setUserInfo] = useState(false);
+
+  const handelMenu = () => setMenu(!Menu);
+  const handelUserInfo = () => setUserInfo(!UserInfo);
+
   const logout = () => {
     signOut(auth);
   };
-return (
-    <Navbar bg="primary" variant="dark" expand="lg" sticky="top">
-    <Container>
-    <LinkContainer to='/'>
-        <img className="w-40 h-15 rounded-md mr-3 cursor-pointer" src="https://i.ibb.co/gj24c9g/logo.jpg" alt="site logo" />
-    </LinkContainer>
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav className="me-auto my-2 my-lg-0" navbarScroll
-      >
-    <LinkContainer to='/'>
-        <Nav.Link>Home</Nav.Link>
-    </LinkContainer>
-      {
-        user ? <>
-    <LinkContainer to='/myproduct'>
-        <Nav.Link>My Product</Nav.Link>
-    </LinkContainer>
-    <LinkContainer to='/manageInventory'>
-        <Nav.Link>Manage Inventory</Nav.Link>
-    </LinkContainer>
-    <LinkContainer to='/addProduct'>
-        <Nav.Link>Add Product</Nav.Link>
-    </LinkContainer>
-        </>:
-        " "
-      }
+  return (
+    <nav className="bg-[#FF8F1B]">
+      <Container>
+        <div className=" lg:grid lg:grid-cols-10 items-center nav_overlay">
+          <NavLink to="/" className=" lg:col-span-2 no-underline">
+            <h2 className="mr-6 mt-2 cursor-pointer text-[30px] text-[#100c08] font-serif font-black">
+              LOGIACA
+            </h2>
+          </NavLink>
+          <div className={Menu ? "nav_bar actives" : " nav_bar"}>
+            <div className="nav_list">
+              <NavLink to="/" className="Nav_link">
+                Home
+              </NavLink>
+              {user ? (
+                <>
+                  <NavLink to="/myproduct" className="Nav_link">
+                    My Product
+                  </NavLink>
+                  <NavLink to="/manageInventory" className="Nav_link">
+                    Manage Inventory
+                  </NavLink>
+                  <NavLink to="/addProduct" className="Nav_link">
+                    Add Product
+                  </NavLink>
+                </>
+              ) : (
+                " "
+              )}
 
-    <LinkContainer to='/blog'>
-        <Nav.Link>Blog</Nav.Link>
-    </LinkContainer>
-    <LinkContainer to='/contact'>
-        <Nav.Link>Contact Us</Nav.Link>
-    </LinkContainer>
-       </Nav>
-     <div className="d-flex nav-login">
-       {
-         user ? <span className="d-flex"><Nav.Link onClick={logout}>Logout</Nav.Link>
-         {
-           user?.photoURL ? <img className="w-10 h-10 rounded-full" src={`${user?.photoURL}`}alt={`${user.displayName.split(" ")[0]}`} />
-           : <img className="w-10 h-10 rounded-full" src= "https://i.ibb.co/kG6vXJx/default-avatar-placeholder-profile-icon-male-vector.jpg " alt={`${user.displayName}`} />
-         }
-         </span>
-         :
-         <LinkContainer to='/login'>
-          <Nav.Link>Login/Register</Nav.Link>
-        </LinkContainer>
-       }
-    
-     </div>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-    );
+              <NavLink to="/blog" className="Nav_link">
+                Blog
+              </NavLink>
+              <NavLink to="/contact" className="Nav_link">
+                Contact Us
+              </NavLink>
+            </div>
+          </div>
+          <div className="lg:col-span-2 Menu_Button_item">
+            <div className="lg:hidden Menu_Button">
+              <span onClick={handelMenu}>
+                {Menu ? <FaTimes /> : <GiHamburgerMenu />}
+              </span>
+            </div>
+            {user ? (
+              <>
+                <span className="d-flex items-center justify-end">
+                  {user?.photoURL ? (
+                    <img
+                      onClick={handelUserInfo}
+                      className="w-10 h-10 rounded-full cursor-pointer"
+                      src={`${user?.photoURL}`}
+                      alt={`${user.displayName.split(" ")[0]}`}
+                    />
+                  ) : (
+                    <img
+                      onClick={handelUserInfo}
+                      className="w-10 h-10 rounded-full cursor-pointer"
+                      src="https://i.ibb.co/kG6vXJx/default-avatar-placeholder-profile-icon-male-vector.jpg "
+                      alt={`${user.displayName}`}
+                    />
+                  )}
+                </span>
+                <div
+                  className={UserInfo ? "user_data block" : "user_data hidden"}
+                >
+                  <span
+                    onClick={logout}
+                    className="flex items-center text-xl font-sans font-semibold cursor-pointer"
+                  >
+                    <AiOutlineLogout className="text-[26px] mr-2" />
+                    {"LogOut"}
+                  </span>
+                </div>
+              </>
+            ) : (
+              <NavLink to="/login" className="nav_login">
+                <BiLogIn className=" text-[35px]" />
+              </NavLink>
+            )}
+          </div>
+        </div>
+      </Container>
+    </nav>
+  );
 };
 
 export default Header;
